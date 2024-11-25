ae325695b2f497077650ece125b539c896db7e56
Guillaume Levasseur <guillaume.levasseur@unity3d.com>
2024-11-25 00:16:41 -0500

    Add more wip

--- Assets/_Game/Levels/10 Performance Testing/SparrowManager.cs
+++ Assets/_Game/Levels/10 Performance Testing/SparrowManager.cs
@@ -1,19 +1,25 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class SparrowManager : MonoBehaviour
 {
     public GameObject ringPrefab; 
     public int NumberOfRings = 10;
+    private int callCount;
+
+    private int[,] grid = new int[1000, 1000];
 
     private Animator animator;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -24,7 +30,7 @@ public class SparrowManager : MonoBehaviour
     
     public void RingLoss(int numberOfRings)
     {
-        
+        callCount++;
         animator?.SetTrigger("RingLost");
         
         for (int i = 0; i < NumberOfRings; i++)
@@ -32,11 +38,30 @@ public class SparrowManager : MonoBehaviour
                 GameObject ring = Instantiate(ringPrefab, transform.position, Quaternion.identity);
                 Rigidbody rb = ring.GetComponent<Rigidbody>();
                 Vector3 randomDirection = Random.insideUnitSphere;
+               
         //
         //     // Introduce a simulated inefficiency (inefficient method call)
-        //     ExpensiveOperation();
+             
         //
-        rb.AddForce(randomDirection * 10, ForceMode.Impulse);
+            rb.AddForce(randomDirection * 10, ForceMode.Impulse);
+            
+            ExpensiveOperation();
         }
+        
+        Debug.Log("Ring loss: " + callCount);
     }
+
+    private void ExpensiveOperation()
+    {
+
+        // for (int x = 0; x < 1000; x++)
+        // {
+        //     for (int y = 0; y < 1000; y++)
+        //     {
+        //         grid[x, y] = Random.Range(0, 10); // Randomly assigned weights
+        //     }
+        // }
+    }
+    
+    
 }
--- Assets/_Game/Levels/10 Performance Testing/Tests/PerformanceTests.cs
+++ Assets/_Game/Levels/10 Performance Testing/Tests/PerformanceTests.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using NUnit.Framework;
 using Unity.PerformanceTesting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.UIElements;
+using Unity.Profiling;
+
 
 public class PerformanceTests
 {
@@ -12,11 +16,17 @@ public class PerformanceTests
     [SetUp]
     public void SetUp()
     {
-        var gameObject = GameObject.Find("Sparrow_LOD0");
-        ringLossEffect = gameObject.GetComponent<SparrowManager>();
+        // Load the prefab from the Assets folder
+        string prefabPath = "Assets/_Game/Levels/10 Performance Testing/Prefabs/Sparrow.prefab";
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+        GameObject instance = GameObject.Instantiate(prefab);
+        string ringPrefabPath = "Assets/_Game/Levels/10 Performance Testing/Prefabs/ring.prefab";
+        GameObject ringPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(ringPrefabPath);
+        ringLossEffect = instance.AddComponent<SparrowManager>();
+        ringLossEffect.ringPrefab = ringPrefab;
+        // var gameObject = GameObject.Find("Sparrow_LOD0");
 
-
-    }
+    } 
     
     // A Test behaves as an ordinary method
     [Test, Performance]
@@ -26,6 +36,7 @@ public class PerformanceTests
             .MeasurementCount(10)
             .IterationsPerMeasurement(5)
             .GC()
+            .ProfilerMarkers("CPU Total Frame Time", "GPU Frame Time", "Inl_UniversalRenderTotal") // Compare this with profiler option, create own marker, find marker for update loop, do graphics renderer example, stonks, figure out graphics performance framework
             .SetUp(()=> {/*setup code*/})
             .CleanUp(()=> {/*cleanup code*/})
             .Run();
@@ -33,11 +44,19 @@ public class PerformanceTests
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
-    [Test, Performance]
-    public IEnumerator PerformanceTestsWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+    [UnityTest, Performance]
+    public IEnumerator PerformanceTestsProfileMarker()
+    { 
+        // Is this the point of the graohics perf tests
+        using(Measure.ProfilerMarkers("CPU Total Frame Time", "GPU Frame Time", "Inl_UniversalRenderTotal"))
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                ringLossEffect.RingLoss(5000);
+                yield return null;
+
+            }
+            
+        }
     }
 }
